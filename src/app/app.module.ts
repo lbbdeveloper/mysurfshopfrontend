f0ab5d3ad2dd7c03d1fef04d391950be11e92e52
a <96930550+lbbdeveloper@users.noreply.github.com>
2022-02-13 11:57:12 -0500

    product list component, product services

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
+import { Routes, RouterModule} from '@angular/router';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/common/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    id: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+
+}
