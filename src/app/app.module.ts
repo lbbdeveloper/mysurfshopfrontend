d2bc51f90f665dabdad38eec1c2a229d5bddcf6a
a <96930550+lbbdeveloper@users.noreply.github.com>
2022-02-13 20:31:19 -0500

    product category component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
 import { Routes, RouterModule} from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
   {path: '', redirectTo: '/products', pathMatch: 'full'},
   {path: '**', redirectTo: '/products', pathMatch: 'full'}
@@ -21,6 +25,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     ProductListComponent,
+    ProductCategoryMenuComponent,
+
   ],
   imports: [
     RouterModule.forRoot(routes),
